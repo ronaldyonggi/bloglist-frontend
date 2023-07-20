22f2a390d059b2f1942a94c17de31f800221693e
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-07-20 16:18:50 -0400

    created Togglable component for toggle visibility

---
+++ src/components/Togglable.js
@@ -0,0 +1,32 @@
+import { forwardRef, useImperativeHandle, useState } from "react"
+
+const Togglable = forwardRef((props, refs) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  useImperativeHandle(refs, () => {
+    return {
+      toggleVisibility
+    }
+  }) 
+
+  return (
+    <div>
+      {!visible && 
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      }
+      {visible && 
+        <div>
+          {props.children}
+          <button onClick={toggleVisibility}>cancel</button>
+        </div>
+
+      }
+    </div>
+  )
+})
+
+export default Togglable
