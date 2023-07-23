a07317ec21bc6451d88b20fc5e37a1fddb339e10
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-07-23 15:27:36 -0400

    add view button to display details of blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const [detail, setDetail] = useState(false)
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      {detail && <button onClick={() => setDetail(!detail)}>hide</button>}
+      {!detail && <button onClick={() => setDetail(!detail)}>view</button>}
+      <div>
+        {detail && blog.url}
+      </div>
+      <div>
+        {detail && <>
+        likes {blog.likes} 
+        <button>like</button>
+        </>}
+      </div>
+      <div>
+        {detail && blog.author}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
