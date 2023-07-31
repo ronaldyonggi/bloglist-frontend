41ec88bd339bbe0a5cb216b7fba8acc1fae33bae
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-07-31 16:10:22 -0400

    removed disable no anonymous default export

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -40,5 +40,4 @@ const deleteBlog = async id => {
   return await axios.delete(`${baseUrl}/${id}`, config)
 }
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, create, setToken, update, deleteBlog }
