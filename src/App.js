bdf9e14b26c5c39728412aa4f643c9d52a3318dd
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-07-31 12:10:44 -0400

    handler for delete a blog

--- src/App.js
+++ src/App.js
@@ -137,15 +137,25 @@ const App = () => {
         .reverse()
         )
     } catch(exception) {
-
     }
   }
+
+  // Handle deleting a blog
+  const handleDelete = async blogObject => {
+    try {
+      await blogService.deleteBlog(blogObject.id)
+      setBlogs(blogs.filter(blog => blog.id !== blogObject.id))
+    } catch(exception) {
+    }
+
+  }
+
 
   // Generate all the blogs
   const generateBlogs = () => (
     <div>
       {blogs.map(blog => 
-        <Blog key={blog.id} blog={blog} handleLike={handleLikeIncrease} />
+        <Blog key={blog.id} blog={blog} handleLike={handleLikeIncrease} user={user} handleDelete={handleDelete} />
         )}
     </div>
   )
