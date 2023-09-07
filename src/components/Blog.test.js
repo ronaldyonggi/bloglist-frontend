0c56cd4120558d94b007afd20a9bc0123862934f
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-09-07 17:06:46 +0700

    eslint formatting

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React from 'react'
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import Blog from './Blog'
 import { render, screen } from '@testing-library/react'
-import Blog from "./Blog";
 
 describe('<Blog />', () => {
 
   test('renders title and author but not URL and number of likes', () => {
     const blogObject = {
-      title: "Testing blog title",
-      author: "Mr Jenkins",
-      url: "www.testingreact.io",
+      title: 'Testing blog title',
+      author: 'Mr Jenkins',
+      url: 'www.testingreact.io',
       likes: 37,
       user: {
         id: 1234
@@ -17,18 +18,18 @@ describe('<Blog />', () => {
     }
 
     const testUser = {
-      username: "testUsername",
-      name:"testName",
-      password: "lol",
-      id: "1234"
+      username: 'testUsername',
+      name:'testName',
+      password: 'lol',
+      id: '1234'
     }
 
     render(<Blog blog={blogObject} user={testUser}/>)
 
     const title = screen.getByText('Testing blog title', { exact: false })
-    const author = screen.getByText('Mr Jenkins', {exact: false})
-    const url = screen.queryByText("www.testingreact.io", {exact:false})
-    const likes = screen.queryByText("37", {exact:false})
+    const author = screen.getByText('Mr Jenkins', { exact: false })
+    const url = screen.queryByText('www.testingreact.io', { exact:false })
+    const likes = screen.queryByText('37', { exact:false })
     expect(title).toBeDefined
     expect(author).toBeDefined
     expect(url).toBeNull
