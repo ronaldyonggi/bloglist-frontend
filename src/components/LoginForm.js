dbdcee42c78076fd09fb748c05da26154e023773
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-10-28 19:27:48 +0700

    created LoginForm component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+const LoginForm = ({
+  login,
+}) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    await login(username, password)
+
+    setUsername('')
+    setPassword('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)}></input>
+        </div>
+
+        <div>
+          password
+          <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)}></input>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
