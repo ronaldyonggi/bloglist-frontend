3b0225c9cadcb4fe8185651d32d372d71369b9f0
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-10-30 08:59:04 +0700

    5.19 use config defined backend address

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,12 +1,12 @@
 describe('Blog app', () => {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`,)
     const user = {
       name: 'Matti Luukkainen',
       username: 'mluukkai',
       password: 'salainen'
     }
-    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.request('POST', `${Cypress.env('BACKEND')}/users/`, user)
     cy.visit('http://localhost:3000')
   })
 
